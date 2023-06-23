79a1bb1771eb28d2c4fc841b71308c8da0a247f5
Yash Sharma <yashsharmaharsh@gmail.com>
2023-06-23 16:16:26 +0530

    comiitt

--- api/index.js
+++ api/index.js
@@ -12,6 +12,12 @@ const multer = require("multer");
 const path = require("path");
 const cors = require('cors');
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://leafy-tapioca-6eb345.netlify.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 dotenv.config();
 
@@ -21,7 +27,6 @@ const connection =mongoose.connect(process.env.MONGO_URL,
     
     if(connection)console.log("Connected to MongoDB");
     
-    app.use(cors);
     app.use("/images", express.static(path.join(__dirname, "public/images")));
     app.use(express.json());
     app.use(helmet());
