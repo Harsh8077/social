92b40f4b2325c1aae9decc6a2eb8a62baa5330d4
Yash Sharma <yashsharmaharsh@gmail.com>
2023-06-24 21:21:54 +0530

    hhh

--- react-social/src/components/post/Post.jsx
+++ react-social/src/components/post/Post.jsx
@@ -20,7 +20,7 @@ export default function Post({ post }) {
 
   useEffect(() => {
     const fetchUser = async () => {
-      const res = await axios.get(`/api/users?userId=${post.userId}`);
+      const res = await axios.get(`/users?userId=${post.userId}`);
       setUser(res.data);
     };
     fetchUser();
@@ -28,7 +28,7 @@ export default function Post({ post }) {
 
   const likeHandler = () => {
     try {
-      axios.put("/api/posts/" + post._id + "/likes", { userId: currentUser._id });
+      axios.put("/posts/" + post._id + "/likes", { userId: currentUser._id });
     } catch (err) {}
     setLike(isLiked ? like - 1 : like + 1);
     setIsLiked(!isLiked);
